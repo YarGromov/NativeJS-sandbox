86c7795c42aa252e710c48061b1c126c2d7e0869
YarGromov <1l1l1arr1l1l1@gmail.com>
2023-01-24 20:15:46 +0100

    handmade map and filter

--- src/array_methods.js
+++ src/array_methods.js
@@ -26,13 +26,56 @@ const students = [
     }
 ];
 
+//map
 
 const getStNames = (array) => {
-    let names = [];
+    let result = [];
+    const getName = item => item.name
     for (let i = 0; i < array.length; i++) {
-        names[i] = array[i].name
+        result[i] = getName(array[i])
     }
-    return names
+    return result
 }
 console.log(getStNames(students))
+
+const getName = item => item.name
+
+const getNewArray = (array, func) => {
+    let result = [];
+    for (let i = 0; i < array.length; i++) {
+        result[i] = func(array[i])
+    }
+    return result
+}
+console.log(getNewArray(students, getName))
+console.log(students.map(getName))
+
+// const addProperty = item => {
+//     item.isStudent = true
+//     return item
+// }
+
+const addProperty = item => ({...item, isStudent: true})
+console.log(addProperty(students))
+
+//filter
+
+const itPush = (array, el) => {
+    array[array.length] = el;
+    return array.length
+}
+
+const itFilter = (array, func) => {
+    const result = [];
+    for (let i = 0; i < array.length; i++) {
+        if (func(array[i]) === true) {
+            itPush(array, array[i])
+        }
+    }
+    return result;
+}
+
+
+
+
 
