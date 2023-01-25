5121b88c5fb5decb913cc5fd21b0eebfe1f157b9
YarGromov <1l1l1arr1l1l1@gmail.com>
2023-01-25 19:13:27 +0100

    deep copy

--- src/basics.js
+++ src/basics.js
@@ -12,7 +12,23 @@ copyUser.age = user.age
 
 const copyUser2 = {...user}
 
-console.log(copyUser2)
+
+const arr = [1,2,3,4,5]
+const copyArr = [...arr]
+
+const students = [
+    {name: 'Bob', age: 34},
+    {name: 'Alex', age: 32}
+]
+
+const shallowCopyStudents = [...students] //shallow copy
+
+const deepCopyStudents = [{...students[0]},{...students[1]}]
+
+
+console.log(students[0] === shallowCopyStudents[0])
+console.log(students === deepCopyStudents)
+
 
 
 
