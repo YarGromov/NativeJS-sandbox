1d3605d88e3675ff9a9ca666f3fce9102760904f
YarGromov <1l1l1arr1l1l1@gmail.com>
2023-01-26 20:43:09 +0100

    addEventListener

---
+++ src/events.js
@@ -0,0 +1,23 @@
+// handler === listener === subscriber === function
+// handler ⊂  callback
+// callback => handler, setTimeout/setInterval, param of arrays methods
+// onclick = handler; handler({...})
+// {...} - event, ev, e
+
+const sm = document.getElementById('small')
+
+const handler1 = (e) => {
+    alert(e)
+}
+const handler2 = (e) => {
+    console.log(e)
+}
+
+
+// sm.onclick = handler1;
+// sm.onclick = handler2;
+// sm.onclick = null;
+
+sm.addEventListener('click', handler1)
+sm.addEventListener('click', handler2)
+sm.removeEventListener('click', handler1)
