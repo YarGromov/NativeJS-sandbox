a2ba6b7a0574849b5f17f3f434d488a618b871fe
YarGromov <1l1l1arr1l1l1@gmail.com>
2023-01-29 16:10:24 +0100

    sort

---
+++ src/sort.js
@@ -0,0 +1,20 @@
+
+
+const strings = ['Donald', 'Alex', 'Bob'];
+
+console.log(strings.sort())
+
+const strings2 = ['Donald', 'Alex', 'Bob', 'alex', 'john', 'Юрий','юлия', '999'];
+console.log(strings2.sort())
+
+console.log(strings2)
+
+console.log(strings2 === strings2.sort())
+
+const numbers = [1000, 25, 777, 6666, 0, -20, '+20']
+
+console.log(numbers.sort())
+
+const sorti = (a, b) => a - b;
+
+console.log(numbers.sort(sorti))
