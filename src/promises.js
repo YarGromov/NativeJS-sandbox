a7c374aa7320ed37e459768de23f6927704e8730
YarGromov <1l1l1arr1l1l1@gmail.com>
2023-02-07 20:00:14 +0100

    async/await

--- src/promises.js
+++ src/promises.js
@@ -193,16 +193,16 @@
 //         console.log('Then')
 //     })
 
-const alex = {
-    askFile() {
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                resolve('Users')
-                // reject(new Error('Can not open file'))
-            }, 2000)
-        })
-    }
-}
+// const alex = {
+//     askFile() {
+//         return new Promise((resolve, reject) => {
+//             setTimeout(() => {
+//                 resolve('Users')
+//                 // reject(new Error('Can not open file'))
+//             }, 2000)
+//         })
+//     }
+// }
 
 // alex.askFile()
 //     .finally(()=>{
@@ -261,29 +261,112 @@ const alex = {
 //     console.log(res)
 // })
 
-const promise = alex.askFile()
-
-promise
-    .then((res) => {
-        console.log(res)
-    })
-    .then((res) => {
-        console.log(res)
-    }) //undefined
-
-promise
-    .then((res) => {
-        console.log(res)
-    })
-
-
+// const promise = alex.askFile()
+//
+// promise
+//     .then((res) => {
+//         console.log(res)
+//     })
+//     .then((res) => {
+//         console.log(res)
+//     }) //undefined
+//
+// promise
+//     .then((res) => {
+//         console.log(res)
+//     })
 
 
+// ===========================  async/await   ===========================
+// const foo = async () => {
+//     console.log(age)
+//     return 10
+// }
+// const promise = foo()
+// console.log(promise)
+// foo()
+//     .then(ten => console.log(ten))
+//     .catch(e => console.log(e))
 
 
+// const foo = async () => { //await
+//     try {
+//         try {
+//             const users = await fetch('http::/facebook.com/users')
+//         } catch (e) {
+//             console.log(e)
+//         }
+//
+//         const user = await fetch('http::/facebook.com/users/1')
+//         const books = await fetch('http::/facebook.com/users/1/books')
+//         const book = await fetch('http::/facebook.com/users/1/books/2')
+//     } catch (e) {
+//         console.log(e)
+//     } finally {
+//         console.log('Finish')
+//     }
+// }
 
 
+// let a = 5;
+// a = a * 2;
+//
+// try {
+//     document.querySelector('.test').innerHTML = a;
+// } catch(err){
+//     console.log(err)
+//     console.log('Error')
+// } finally {
+//     console.log('Program completed')
+// }
+//
+// console.log(a)
+// t1()
+//
+// function t1 (){
+//     console.log('Hello')
+// }
 
+const delay = (ms) => {
+    return new Promise(r => setTimeout(()=> r(), ms))
+}
 
+let url = 'https://jsonplaceholder.typicode.com/todos/1';
+let urlComments = "https://jsonplaceholder.typicode.com/comments/1"
 
+// function fetchTodos(){
+//     console.log('Fetch todo started...')
+//      return delay(2000).then(()=>fetch(url))
+//     .then(response => response.json())
+// }
+//
+// fetchTodos()
+//     .then(data => {
+//         console.log("Data: ", data)
+//     })
+//     .catch(e => console.error(e))
+
+async function  fetchAsyncTodos(){
+    console.log('Fetch todos started...')
+    try {
+        await delay(2000)
+        const response = await fetch(url)
+        const data = await response.json()
+        console.log("Data: ", data)
+    } catch (e){
+        console.error(e)
+    } finally {
+        console.log("Finish")
+    }
+}
+fetchAsyncTodos()
+
+// async function fetchAsyncComments(){
+//     console.log('Fetch comments started...')
+//     await delay(3000)
+//     const resp = await fetch(urlComments)
+//     const data = await resp.json()
+//     console.log(data)
+// }
+// fetchAsyncComments()
 
