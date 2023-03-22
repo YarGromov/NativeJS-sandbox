78526bfe28de6430c88a106bcc22328e9b4ebdcb
YarGromov <1l1l1arr1l1l1@gmail.com>
2023-03-22 19:08:20 +0100

    task6

--- src/promises/promises2.js
+++ src/promises/promises2.js
@@ -18,15 +18,53 @@
 //     console.log("Subscriber 2: " + res)
 // })
 
+// const doAfter = (seconds) => {
+//   let newPromise =  new Promise((res)=>{
+//         setTimeout(()=>{
+//             res()
+//         }, seconds*1000)
+//     })
+//     return newPromise
+// }
+
+// doAfter(5).then( () => console.log('я сработал через 5 секунд') );
+// doAfter(3).then( () => console.log('я сработал через 3 секунд') );
+// doAfter(10).then( () => console.log('я сработал через 10 секунд') );
+
+// let promise3 = doAfter(3);
+// promise3.then( () => console.log('я сработал через 3 секунд') );
+// promise3.then( () => console.log('и я тоже следом сработал через 3 секунд') );
+// promise3.then( () => console.log('и я') );
+
+
+
 const doAfter = (seconds) => {
-  let newPromise =  new Promise((res)=>{
+    let newPromise =  new Promise((res, rej)=>{
         setTimeout(()=>{
-            res()
+            const randValue = Math.random()
+            if (randValue > 0.5){
+                res(randValue)
+            } else{
+                rej(randValue)
+            }
         }, seconds*1000)
     })
     return newPromise
 }
+doAfter(2).then(()=> console.log('result')).catch(()=>console.log('Error'))
 
-doAfter(5).then( () => console.log('я сработал через 5 секунд') );
-doAfter(3).then( () => console.log('я сработал через 3 секунд') );
-doAfter(10).then( () => console.log('я сработал через 10 секунд') );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
