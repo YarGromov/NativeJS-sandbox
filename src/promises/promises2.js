56d9190abc6065ffa187b820df75fecc562fd29a
YarGromov <1l1l1arr1l1l1@gmail.com>
2023-03-22 18:45:18 +0100

    task5

--- src/promises/promises2.js
+++ src/promises/promises2.js
@@ -1,21 +1,32 @@
+//
+// let myPromise = new Promise((resolve, reject)=>{
+//   setTimeout(()=>{
+//       const randValue = Math.random()
+//       resolve(randValue)
+//   },2000)
+//     console.log(1)
+//     console.log(2)
+//     console.log(3)
+// })
+// console.log(myPromise)
+//
+// myPromise.then((res)=>{
+//     console.log("Subscriber 1: " + res)
+// })
+//
+// myPromise.then((res)=>{
+//     console.log("Subscriber 2: " + res)
+// })
 
-let myPromise = new Promise((resolve, reject)=>{
-  setTimeout(()=>{
-      const randValue = Math.random()
-      resolve(randValue)
-  },2000)
-    console.log(1)
-    console.log(2)
-    console.log(3)
-})
-console.log(myPromise)
+const doAfter = (seconds) => {
+  let newPromise =  new Promise((res)=>{
+        setTimeout(()=>{
+            res()
+        }, seconds*1000)
+    })
+    return newPromise
+}
 
-myPromise.then((res)=>{
-    console.log("Subscriber 1: " + res)
-})
-
-myPromise.then((res)=>{
-    console.log("Subscriber 2: " + res)
-})
-
-
+doAfter(5).then( () => console.log('я сработал через 5 секунд') );
+doAfter(3).then( () => console.log('я сработал через 3 секунд') );
+doAfter(10).then( () => console.log('я сработал через 10 секунд') );
