8336e4da6a7e81e49c6a780979fa854e78b4a52c
YarGromov <1l1l1arr1l1l1@gmail.com>
2023-03-22 18:01:56 +0100

    task1

--- src/promises/promises2.js
+++ src/promises/promises2.js
@@ -1,8 +1,12 @@
 
-let myPromise = new Promise(()=>{
-    alert('Hello')
+let myPromise = new Promise((resolve, reject)=>{
+    resolve()
+    console.log(1)
+    console.log(2)
+    console.log(3)
 })
 
 myPromise.then(()=>{
     console.log("My promise was resolved")
 })
+
