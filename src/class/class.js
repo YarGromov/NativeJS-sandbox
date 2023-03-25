89522bf7bf35ce75d7144bf47702fca27244be52
YarGromov <1l1l1arr1l1l1@gmail.com>
2023-03-25 22:21:51 +0100

    encapsulation

--- src/class/class.js
+++ src/class/class.js
@@ -1,9 +1,19 @@
 class User {
+    #name = '';
     constructor(name, site, dob) {
-        this.name = name;
+        this.#name = name;
         this.site = site;
         this.dateOfBirth = dob;
         this.counter = 0;
+    }
+
+    get name(){
+        return this.#name
+    }
+
+    set name(value){
+        if (value === 'Igor') throw new Error('Igor is not allowed name')
+        this.#name = value;
     }
 
     hello(){
@@ -16,5 +26,5 @@ const u111 = new User('Dimych', 'it-incubator.by', new Date(1988, 1, 2))
 const u222 = new User('Artem', 'it-incubator.by', new Date(1989, 10, 12))
 
 
-u111.hello()
-u111.hello()
+u111.name = 'Igor1'
+console.log(u111.name)
